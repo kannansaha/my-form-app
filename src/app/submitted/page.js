de37aea7d7e2442e68e1b70beeaa80ddc5a3dd96
kannan <kannasaha8940@gmail.com>
2024-05-12 19:12:04 +0530

    f-changed

--- src/app/submitted/page.js
+++ src/app/submitted/page.js
@@ -1,23 +1,24 @@
 "use client"
-import { Suspense} from "next/navigation";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams} from 'next/navigation';
 import "./submitted.css";
+import { Suspense } from 'react';
 
 export default function Submitted() {
   const searchParams = useSearchParams();
-  // const name  =searchParams.get("name");
-  // const email  =searchParams.get("email");
-  // const message =searchParams.get("message");
+  const name  =searchParams.get("name");
+  const email  =searchParams.get("email");
+  const message =searchParams.get("message");
+
+
 
   return (
-    
+    <Suspense>
     <div className='Submitted-data'>
       <h1>Submitted Data</h1>
-      <Suspense  fallback={<div>Loading...</div>}>
-      <p>Name: {searchParams.get("name")}</p>
-      <p>Email: {searchParams.get("email")}</p>
-      <p>Message: {searchParams.get("message")}</p>
-      </Suspense>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+      <p>Message: {message}</p>
     </div>
+    </Suspense>
   );
 }
